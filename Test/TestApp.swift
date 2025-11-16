dc651f9dd2ec7d40e6e1b23305f0e2beb278e314
ishiokakazuma <6301043919@g.ecc.u-tokyo.ac.jp>
2025-11-16 21:09:12 +0900

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Ishioka Kazuma on 2025/11/16.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
